53fc2a8e2c9ba0383f2e29dd8770897ac201b23c
Sung Won Cho <mikeswcho@gmail.com>
2015-07-22 17:11:25 +1000

    Bring setup and teardown to beforeEach and afterEach hooks to make sure they run always

--- packages/topic/tests/topic_tests.js
+++ packages/topic/tests/topic_tests.js
@@ -1,9 +1,18 @@
 describe("submitTopic", function(){
-  it("creates a topic", function(){
-    // Setup
+  // Setup
+  beforeEach(function () {
     sinon.stub(Meteor, 'userId', function () { return 1; });
     sinon.stub(Meteor, 'user', function () { return {username: 'jon'}; });
+  });
 
+  // Teardown
+  afterEach(function () {
+    Topics.remove({});
+    Meteor.userId.restore();
+    Meteor.user.restore();
+  });
+
+  it("creates a topic", function(){
     var topic = {
       title: 'testTitle',
       body: 'testBody',
@@ -13,10 +22,5 @@ describe("submitTopic", function(){
     Meteor.call('submitTopic', topic);
 
     expect(Topics.find().count()).to.equal(1);
-
-    // Teardown
-    Topics.remove({});
-    Meteor.userId.restore();
-    Meteor.user.restore();
   });
 });
